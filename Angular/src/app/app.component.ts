53fd6d39ce06a3bb5ae58013a083a592040abd90
Sonia Bullah <sonia.bullah@entelect.co.za>
2023-03-24 19:36:04 +0200

    Added front-end for displaying book details

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Books } from 'src/models/books';
+import { BooksData } from 'src/models/booksData';
+import { BooksDataService } from './services/books/books-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular';
+
+  titles: Books[] = [];
+  //dataSource: MatTableDataSource<Books>;
+
+  constructor(private booksDataService: BooksDataService){
+    //this.dataSource = new MatTableDataSource<Books>();
+    this.titles = [];
+  }
+
+  ngOnInit(): void {
+
+    this.booksDataService.getBooks().subscribe((data: Books[]) => {
+      this.titles = data;
+    });
+}
+
+// getBooks(): void {
+//   this.booksDataService.getBooks().subscribe((data: BooksData) => {
+//     this.dataSource.data = data.data;
+//   });
+// }
 }
--- JavaAndSpringIncubator/src/main/java/com/example/JavaAndSpringIncubator/controllers/BooksController.java
+++ JavaAndSpringIncubator/src/main/java/com/example/JavaAndSpringIncubator/controllers/BooksController.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ public class BooksController {
     }
 
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<BooksDTO>> getBooks()
     {
